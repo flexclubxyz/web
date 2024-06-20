de980c03ba017f1a2e490732b1ea8242085ce025
ashrafstakala <ashrafstakala@gmail.com>
2024-06-20 11:54:44 +0200

    home page styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,35 @@
 "use client";
 import Link from "next/link";
 import { ConnectButton } from "@rainbow-me/rainbowkit";
-import { useAccount } from "wagmi";
+import { useAccount, useDisconnect } from "wagmi";
+import { useEffect, useState } from "react";
+import { switchChain, readContract } from "@wagmi/core";
+import { base } from "@wagmi/core/chains";
+import { config } from "@/wagmi";
+import { contractABI, contractAddress } from "@/config";
 
 export default function HomePage() {
-  const { status } = useAccount();
+  const { status, address, chain } = useAccount();
+  const { disconnect } = useDisconnect();
+  const [effectiveBalance, setEffectiveBalance] = useState(0);
+  const [isLoading, setIsLoading] = useState(false);
 
   const flexclubs = [
     {
       id: "0xFlex001",
-      name: "Flexclub 001",
-      goal: "Save for Devcon 2024 in Bangkok",
+      name: "FLEXCLUB 001",
+      goal: "Devcon 2024 in Bangkok 🇹🇭",
       target: 800,
       pooled: 144.79,
       flexers: 7,
     },
     {
-      id: "002",
-      name: "Flexclub 002",
-      goal: "Save for a group vacation",
-      target: 10000,
-      pooled: 7000,
-      flexers: 15,
+      id: "0xFlex001",
+      name: "FLEXCLUB 002",
+      goal: "Farcon 2025 in Tokyo 🇯🇵",
+      target: 5000,
+      pooled: 8000,
+      flexers: 450,
     },
   ];
 
@@ -34,6 +42,31 @@ export default function HomePage() {
     }).format(value); // Assuming value is in USDC format already
   };
 
+  const fetchEffectiveBalance = async () => {
+    if (address) {
+      try {
+        setIsLoading(true);
+        const balance = await readContract(config, {
+          abi: contractABI,
+          address: contractAddress,
+          functionName: "getEffectiveBalance",
+          args: [address],
+        });
+        setEffectiveBalance(Number(balance));
+      } catch (error) {
+        console.error("Error fetching effective balance:", error);
+      } finally {
+        setIsLoading(false);
+      }
+    }
+  };
+
+  useEffect(() => {
+    if (status === "connected") {
+      fetchEffectiveBalance();
+    }
+  }, [status, address]);
+
   return (
     <div className="max-w-lg mx-auto bg-gray-800 text-white rounded-lg shadow-md p-6 mt-4">
       <h2 className="text-2xl font-bold mb-4 text-center">
@@ -42,6 +75,12 @@ export default function HomePage() {
       <p className="mb-6 text-center">
         Onchain goal-based saving clubs. The fun way to save onchain!
       </p>
+      {/* {status === "connected" && (
+        <div className="balance-section bg-gray-900 p-4 rounded-md mb-6 text-center">
+          <h4 className="balance-header">Flexclub Balance 🤑</h4>
+          <p>{formatUSDC(effectiveBalance)} USDC</p>
+        </div>
+      )} */}
       <div>
         <div className="space-y-4">
           {flexclubs.map((club) => (
@@ -61,7 +100,7 @@ export default function HomePage() {
                 🌀
               </p>
               <Link href={`/${club.id}`} className="link">
-                Join Flexclub
+                Join this club
               </Link>
             </div>
           ))}
@@ -81,6 +120,17 @@ export default function HomePage() {
                 showBalance={false}
               />
             </div>
+          </div>
+        )}
+        {status === "connected" && (
+          <div className="flex justify-center mt-8">
+            <ConnectButton
+              accountStatus={{
+                smallScreen: "full",
+                largeScreen: "full",
+              }}
+              showBalance={true}
+            />
           </div>
         )}
       </div>
