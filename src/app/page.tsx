676b9020c5cc7f75300ff5466f59ea3786c7a027
ashrafstakala <ashrafstakala@gmail.com>
2024-06-22 18:33:21 +0200

    read smart contract on home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,73 +1,12 @@
 "use client";
 import Link from "next/link";
 import { ConnectButton } from "@rainbow-me/rainbowkit";
-import { useAccount, useDisconnect } from "wagmi";
-import { useEffect, useState } from "react";
-import { switchChain, readContract } from "@wagmi/core";
-import { base } from "@wagmi/core/chains";
-import { config } from "@/wagmi";
-import { contractABI, contractAddress } from "@/config";
+import { useAccount } from "wagmi";
+import ClubCard001 from "@/components/ClubCard001";
+import ClubCard002 from "@/components/ClubCard002";
 
 export default function HomePage() {
-  const { status, address, chain } = useAccount();
-  const { disconnect } = useDisconnect();
-  const [effectiveBalance, setEffectiveBalance] = useState(0);
-  const [isLoading, setIsLoading] = useState(false);
-
-  const flexclubs = [
-    {
-      id: "0xFlex001",
-      name: "FLEXCLUB 001",
-      goal: "Devcon in Bangkok 🇹🇭",
-      target: 800,
-      pooled: 129.84,
-      flexers: 7,
-      apy: 6.61, // Add APY information
-    },
-    {
-      id: "0xFlex002",
-      name: "FLEXCLUB 002",
-      goal: "Farcon 2025 (location tbd) 🇯🇵",
-      target: 5000,
-      pooled: 17,
-      flexers: 2,
-      apy: 6.61, // Add APY information
-    },
-  ];
-
-  const formatUSDC = (value: number) => {
-    return new Intl.NumberFormat("en-US", {
-      style: "currency",
-      currency: "USD",
-      minimumFractionDigits: 2,
-      maximumFractionDigits: 4,
-    }).format(value); // Assuming value is in USDC format already
-  };
-
-  const fetchEffectiveBalance = async () => {
-    if (address) {
-      try {
-        setIsLoading(true);
-        const balance = await readContract(config, {
-          abi: contractABI,
-          address: contractAddress,
-          functionName: "getEffectiveBalance",
-          args: [address],
-        });
-        setEffectiveBalance(Number(balance));
-      } catch (error) {
-        console.error("Error fetching effective balance:", error);
-      } finally {
-        setIsLoading(false);
-      }
-    }
-  };
-
-  useEffect(() => {
-    if (status === "connected") {
-      fetchEffectiveBalance();
-    }
-  }, [status, address]);
+  const { status } = useAccount();
 
   return (
     <div className="max-w-lg mx-auto bg-gray-800 text-white rounded-lg shadow-md p-6 mt-4">
@@ -79,31 +18,8 @@ export default function HomePage() {
       </p>
       <div>
         <div className="space-y-4">
-          {flexclubs.map((club) => (
-            <div key={club.id} className="card">
-              <h3>{club.name}</h3>
-              <p>{club.goal}</p>
-              <p>
-                <span className="font-semibold">Target:</span>{" "}
-                {formatUSDC(club.target)} USDC 🎯
-              </p>
-              <p>
-                <span className="font-semibold">Pooled:</span>{" "}
-                {formatUSDC(club.pooled)} USDC 💰
-              </p>
-              <p>
-                <span className="font-semibold">Members:</span> {club.flexers}{" "}
-              </p>
-              <p>
-                <span className="font-semibold">APY:</span> {club.apy}% 📈
-              </p>
-              <Link href={`/${club.id}`} legacyBehavior>
-                <a className="mt-4 inline-block bg-blue-500 text-white py-2 px-4 rounded ">
-                  Join this club 🙌
-                </a>
-              </Link>
-            </div>
-          ))}
+          <ClubCard001 />
+          <ClubCard002 />
         </div>
         {status !== "connected" && (
           <div className="text-center mt-8">
