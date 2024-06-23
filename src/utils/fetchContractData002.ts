80e0c06907436569a4c5eb8c645938447d9caa59
ashrafstakala <ashrafstakala@gmail.com>
2024-06-23 13:07:10 +0200

    fix flexclub002 transaction fetch function

--- src/utils/fetchContractData002.ts
+++ src/utils/fetchContractData002.ts
@@ -10,7 +10,7 @@ export const fetchTransactions = async (
   contractAddress: string
 ): Promise<Log[]> => {
   const apiKey = process.env.NEXT_PUBLIC_BASESCAN_API_KEY;
-  const url = `https://api.basescan.org/api?module=logs&action=getLogs&address=$0xcE51BE974FBE7e642072cAdb87F3F63b80cD7c8E&fromBlock=0&toBlock=latest&apikey=${apiKey}`;
+  const url = `https://api.basescan.org/api?module=logs&action=getLogs&address=0xcE51BE974FBE7e642072cAdb87F3F63b80cD7c8E&fromBlock=0&toBlock=latest&apikey=${apiKey}`;
   const response = await axios.get(url);
 
   if (response.data.result && Array.isArray(response.data.result)) {
