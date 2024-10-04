7656ae71669a277accbb5aa662a60f81e47f6325
ashrafstakala <ashrafstakala@gmail.com>
2024-10-04 16:38:54 +0200

    donation refund component

--- src/components/DonationRefund.tsx
+++ src/components/DonationRefund.tsx
@@ -1,40 +1,57 @@
 import React, { useState } from "react";
-import { useContractWrite, usePrepareContractWrite } from "wagmi";
+import { useMutation } from "@tanstack/react-query";
+import { useAccount } from "wagmi";
+import { writeContract } from "@wagmi/core";
+import { config } from "@/wagmi";
 
-export function Refund({ contractAddress, contractABI, onRefundSuccess }) {
+export function DonationRefund({
+  contractABI,
+  contractAddress,
+  onRefundSuccess,
+}: {
+  contractABI: any;
+  contractAddress: `0x${string}`;
+  onRefundSuccess: () => void;
+}) {
+  const { address } = useAccount();
   const [loading, setLoading] = useState(false);
 
-  const { config } = usePrepareContractWrite({
-    address: contractAddress,
-    abi: contractABI,
-    functionName: "refund",
-  });
+  const { mutate } = useMutation({
+    mutationFn: async () => {
+      if (!address) {
+        return;
+      }
 
-  const { write } = useContractWrite({
-    ...config,
-    onSuccess() {
-      onRefundSuccess();
-    },
-    onError(error) {
-      console.error("Error during refund:", error);
+      try {
+        setLoading(true);
+        // Proceed with the refund
+        await writeContract(config, {
+          abi: contractABI,
+          address: contractAddress,
+          functionName: "refund",
+          account: address,
+          args: [], // Include args, even if empty
+        });
+        onRefundSuccess();
+      } catch (error) {
+        console.error("Error during contract interaction:", error);
+      } finally {
+        setLoading(false);
+      }
     },
   });
-
-  const handleRefund = () => {
-    setLoading(true);
-    write?.();
-    setLoading(false);
-  };
 
   return (
-    <div className="mt-4">
-      <button
-        onClick={handleRefund}
-        disabled={loading || !write}
-        className="p-2 bg-red-600 rounded-md text-white w-full"
-      >
-        {loading ? "Processing..." : "Request Refund"}
-      </button>
-    </div>
+    <button
+      onClick={() => mutate()}
+      className="p-2 bg-red-600 rounded-md text-white hover:bg-red-700 w-full"
+      disabled={loading}
+    >
+      {loading ? (
+        <div className="loader border-t-transparent border-4 border-white rounded-full w-4 h-4 mx-auto"></div>
+      ) : (
+        "Request Refund"
+      )}
+    </button>
   );
 }
